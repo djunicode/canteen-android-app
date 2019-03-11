8f13a0ed9b5c27df59f8c3e9c82816e2c40611d9
Harsh Mehta <harshmehta51099@gmail.com>
2019-03-11 18:21:19 +0530

    Reverted Changed and Added Category Request and Response classes

--- app/src/main/java/io/github/djunicode/canteenapp/RequestObjects/Category.java
+++ app/src/main/java/io/github/djunicode/canteenapp/RequestObjects/Category.java
@@ -1,4 +1,14 @@
 package io.github.djunicode.canteenapp.RequestObjects;
 
 public class Category {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
