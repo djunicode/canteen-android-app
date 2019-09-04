f89ec12eddd12daafb7347837dd0771e7c044d58
Arnav Parekhji <arnav17121999@gmail.com>
2019-09-04 18:12:26 +0530

    Login, Session Management and Logout completed

--- app/src/main/java/io/github/djunicode/canteenapp/RequestObjects/SignInRequest.java
+++ app/src/main/java/io/github/djunicode/canteenapp/RequestObjects/SignInRequest.java
@@ -1,7 +1,12 @@
 package io.github.djunicode.canteenapp.RequestObjects;
 
+import com.google.gson.annotations.SerializedName;
+
 public class SignInRequest {
-    private String sap, password;
+    @SerializedName("username")
+    private String sap;
+    @SerializedName("password")
+    private String password;
 
     public SignInRequest(String sap, String password) {
         this.sap = sap;
--- app/src/main/java/io/github/djunicode/canteenapp/RetrofitInterfaces/ApiInterface.java
+++ app/src/main/java/io/github/djunicode/canteenapp/RetrofitInterfaces/ApiInterface.java
@@ -2,9 +2,11 @@ package io.github.djunicode.canteenapp.RetrofitInterfaces;
 
 import java.util.List;
 
+import io.github.djunicode.canteenapp.RequestObjects.ChangePassword;
 import io.github.djunicode.canteenapp.RequestObjects.SignInRequest;
 import io.github.djunicode.canteenapp.ResponseObjects.SignInResponse;
 import io.github.djunicode.canteenapp.models.MenuItem;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -12,11 +14,15 @@ import retrofit2.http.POST;
 
 public interface ApiInterface {
 
-    @POST("login/")
+    @POST("auth/token/login/")
     Call<SignInResponse> createPostLogin(@Body SignInRequest signInRequest);
 
 
     @GET("menu/")
     Call<List<MenuItem>> getMenu();
+
+
+    @POST("auth/users/set_password/")
+    Call<ResponseBody> changePassword(@Body ChangePassword changePassword);
 
 }
